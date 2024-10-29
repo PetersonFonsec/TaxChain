2f7dde509abcded42d7eb45e424bfa5c151678c5
peterson <petersonsimiao1@gmail.com>
2024-10-28 22:29:00 -0300

    feat: criando a lp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <main id={styles.page}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            <strong>Não Complica! </strong>
+            Faça Sua Declaração de Cripto em Minutos.
+          </h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <ol className={styles.list}>
+            <li>
+              1 ° Entre na sua corretora e exporte o extrato das suas movimentações do ultimo ano.
+            </li>
+
+            <li>
+              2 ° Preencha o formulário ao lado para receber o seu link, com  o uma pagina unica e exclusiva sua.
+            </li>
+
+            <li>
+            <strong>3° Pronto! </strong>Agora é só aguardar que enviaremos por email um link com acesso  valido durante 3 dias.
+            </li>
+          </ol>
         </div>
+
+        <form className={styles.form}>
+          <h2>Informe seu contato</h2>
+          
+          <label htmlFor="email">
+            <input className={styles.input} id="email" placeholder="E-mail" type="text" />
+          </label>
+
+          <label htmlFor="celphone">
+            <input className={styles.input} id="celphone" placeholder="Telefone" type="text" />
+          </label>
+
+          <button className={styles.button}>Upload do extrato</button>
+
+          <legend>Por enquanto funcionamos apenas com o formato de extrato da bynance.</legend>
+        </form>
       </main>
+
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
-    </div>
+    </>
   );
 }
